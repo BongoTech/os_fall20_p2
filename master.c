3f8216c98e97e94323000018542fea93484ffbfc
BongoTech <c_mckiel@yahoo.com>
2020-10-04 18:52:40 -0500

    Added some cmd line processing. master does not run.

--- master.c
+++ master.c
@@ -20,10 +20,66 @@
 //          Type: gcc -Wall -g master.c -o master
 //*****************************************************
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int help(char*);
 
 int main(int argc, char *argv[])
 {
+    int max_lifetime_children = 4;
+    int max_concurrent_children = 2;
+    int max_run_time = 100;
+
+
+    int option;
+    while ( (option = getopt(argc, argv, ":n:s:t:h")) != 1 ) {
+        switch ( option ) {
+            case 'h':
+                help(argv[0]);
+                return 0;
+            case ':':
+                fprintf(stderr, "%s: Error: Missing argument value.\n%s\n", argv[0], strerror(errno));
+                return 1;
+            case 'n':
+                max_lifetime_children = atoi(optarg);
+                break;
+            case 's':
+                max_concurrent_children = atoi(optarg);
+                break;
+            case 't':
+                max_run_time = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "%s: Error: Unknown argument.\n%s\n", argv[0], strerror(errno));
+                return 1;
+        }
+    }
+
+    if ( max_concurrent_children < 1 || max_concurrent_children > 20 ) {
+        fprintf(stderr, "%s: Error: s must satisfy 1 <= s <= 20.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    if ( max_lifetime_children < 1 ) {
+        fprintf(stderr, "%s: Error: n must be at least 1.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+    if ( max_run_time < 1 ) {
+        fprintf(stderr, "%s: Error: n must be at least 1.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
+
+
+
+
+
+
     printf("Hello from master.\n");
     return 0;
 }
