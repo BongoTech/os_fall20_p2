c5ce1d3c5a9ce8b9c6cb5c73ae061737687c286e
BongoTech <c_mckiel@yahoo.com>
2020-10-05 10:04:36 -0500

    Finished command line processing.

--- master.c
+++ master.c
@@ -2,7 +2,7 @@
 //Program Name: master
 //Author: Cory Mckiel
 //Date Created: Oct 4, 2020
-//Last Modified: Oct 4, 2020
+//Last Modified: Oct 5, 2020
 //Program Description:
 //      Assignment two for operating systems class 4760
 //      during fall20 semester.
@@ -34,10 +34,13 @@ int main(int argc, char *argv[])
     int max_lifetime_children = 4;
     int max_concurrent_children = 2;
     int max_run_time = 100;
+    //FILE *fp = NULL;
+    char file[51];
 
-
+//BEGIN: Command line processing.
+//*****************************************************
     int option;
-    while ( (option = getopt(argc, argv, ":n:s:t:h")) != 1 ) {
+    while ( (option = getopt(argc, argv, ":n:s:t:h")) != -1 ) {
         switch ( option ) {
             case 'h':
                 help(argv[0]);
@@ -54,7 +57,7 @@ int main(int argc, char *argv[])
             case 't':
                 max_run_time = atoi(optarg);
                 break;
-            default:
+            case '?':
                 fprintf(stderr, "%s: Error: Unknown argument.\n%s\n", argv[0], strerror(errno));
                 return 1;
         }
@@ -71,15 +74,38 @@ int main(int argc, char *argv[])
     }
 
     if ( max_run_time < 1 ) {
-        fprintf(stderr, "%s: Error: n must be at least 1.\n%s\n", argv[0], strerror(errno));
+        fprintf(stderr, "%s: Error: t must be at least 1.\n%s\n", argv[0], strerror(errno));
         return 1;
     }
 
+    if ( optind < argc ) {
+        strncpy(file, argv[optind], 50);
+    } else {
+        fprintf(stderr, "%s: Error: There must be a file name.\n%s\n", argv[0], strerror(errno));
+        return 1;
+    }
 
+    printf("s: %d\n", max_concurrent_children);
 
+    printf("n: %d\n", max_lifetime_children);
 
+    printf("t: %d\n", max_run_time);
 
+    printf("file: %s\n", file);
 
-    printf("Hello from master.\n");
+//END: Command line processing.
+//*****************************************************
+
+    return 0;
+}
+
+int help(char *prog_name)
+{
+    printf("%s: Usage: %s [-n x] [-s y] [-t z] input_file\n", prog_name, prog_name);
+    printf("Where: x is number of children %s makes over lifetime.(Default 4)\n", prog_name);
+    printf("y is max number of children existing at one time.(Default 2)\n");
+    printf("z is max time the entire process takes. (Default 100.)\n");
+    printf("input_file is some file containing a list of strings, one per line.\n");
+    printf("input_file is not optional.\n");
     return 0;
 }
